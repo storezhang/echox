3891225b84a0180a08419eb81823ea46208d0a51
tw-chen <chentw@class100.com>
2020-06-28 12:24:21 +0800

    添加手机号验证

--- validator.go
+++ validator.go
@@ -1,14 +1,14 @@
 package echox
 
 import (
-	"strings"
-
 	"github.com/go-playground/locales/en"
 	"github.com/go-playground/locales/zh"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 	enLang "github.com/go-playground/validator/v10/translations/en"
 	zhLang "github.com/go-playground/validator/v10/translations/zh"
+	"regexp"
+	"strings"
 )
 
 var (
@@ -20,12 +20,27 @@ type customValidator struct {
 	validator *validator.Validate
 }
 
+// 检查手机号
+func CheckMobile(mobile string) bool {
+	regular := `^861([38][0-9]|14[579]|5[^4]|16[6]|7[1-35-8]|9[189])\d{8}$`
+	reg := regexp.MustCompile(regular)
+
+	return reg.MatchString(mobile)
+}
+
+// 自定义手机号验证函数
+func checkMobile(fl validator.FieldLevel) bool {
+
+	return CheckMobile(fl.Field().String())
+}
+
 func (cv *customValidator) Validate(i interface{}) error {
 	return cv.validator.Struct(i)
 }
 
 func initValidate() {
 	v = validator.New()
+	v.RegisterValidation("mobile", checkMobile)
 
 	translator = ut.New(en.New(), en.New(), zh.New())
 	if en, success := translator.GetTranslator("en"); success {
