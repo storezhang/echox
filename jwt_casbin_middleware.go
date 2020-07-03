8696e18630e1052acd0aecf0914e49e4915584dd
storezhang <storezhang@gmail.com>
2020-07-03 17:06:16 +0800

    Casbin增加尾部斜杠

--- jwt_casbin_middleware.go
+++ jwt_casbin_middleware.go
@@ -2,6 +2,7 @@ package echox
 
 import (
 	"net/http"
+	`strings`
 
 	"github.com/casbin/casbin/v2"
 	"github.com/labstack/echo/v4"
@@ -10,9 +11,14 @@ import (
 
 type (
 	JWTCasbinConfig struct {
-		Skipper  middleware.Skipper
+		// 确定是不是要走中间件
+		Skipper middleware.Skipper
+		// Casbin的权限验证模块
 		Enforcer *casbin.Enforcer
-		JWT      *JWTConfig
+		// JWT的配置
+		JWT *JWTConfig
+		// 是否包含尾部斜杠
+		TrailingSlash bool
 	}
 )
 
@@ -20,6 +26,7 @@ var (
 	DefaultJWTCasbinConfig = JWTCasbinConfig{
 		Skipper: middleware.DefaultSkipper,
 	}
+
 	MethodMapping = map[string]string{
 		"GET":    "r",
 		"POST":   "c",
@@ -29,10 +36,11 @@ var (
 	}
 )
 
-func JWTCasbinMiddleware(e *casbin.Enforcer, jwt *JWTConfig) echo.MiddlewareFunc {
+func JWTCasbinMiddleware(e *casbin.Enforcer, jwt *JWTConfig, trailingSlash bool) echo.MiddlewareFunc {
 	c := DefaultJWTCasbinConfig
 	c.Enforcer = e
 	c.JWT = jwt
+	c.TrailingSlash = trailingSlash
 
 	return JWTCasbinWithConfig(c)
 }
@@ -64,9 +72,17 @@ func (jcc *JWTCasbinConfig) CheckPermission(c echo.Context) (bool, error) {
 		Context: c,
 		JWT:     jcc.JWT,
 	}
+
+	path := c.Request().URL.Path
+	// 取得Path
+	// 统一加上最后的斜杠
+	if jcc.TrailingSlash && !strings.HasSuffix(path, "/") {
+		path += "/"
+	}
+
 	if user, err := ec.User(); nil != err {
 		return false, err
 	} else {
-		return jcc.Enforcer.Enforce(user.IdString(), c.Request().URL.Path, MethodMapping[c.Request().Method])
+		return jcc.Enforcer.Enforce(user.IdString(), path, MethodMapping[c.Request().Method])
 	}
 }
