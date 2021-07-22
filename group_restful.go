9ace02802b2fe4724ad3826c208d9a8e618405ee
storezhang <storezhang@gmail.com>
2021-07-22 21:21:58 +0800

    修复Restful get失败时的状态码

--- group_restful.go
+++ group_restful.go
@@ -8,19 +8,35 @@ import (
 )
 
 func (g *Group) RestfulPost(path string, handler restfulHandler, middlewares ...MiddlewareFunc) *Route {
-	return g.restful(http.MethodPost, path, handler, http.StatusCreated, http.StatusBadRequest, middlewares...)
+	return g.restful(
+		http.MethodPost, path, handler,
+		http.StatusCreated, http.StatusBadRequest,
+		middlewares...,
+	)
 }
 
 func (g *Group) RestfulGet(path string, handler restfulHandler, middlewares ...MiddlewareFunc) *Route {
-	return g.restful(http.MethodGet, path, handler, http.StatusOK, http.StatusNotFound, middlewares...)
+	return g.restful(
+		http.MethodGet, path, handler,
+		http.StatusOK, http.StatusNoContent,
+		middlewares...,
+	)
 }
 
 func (g *Group) RestfulPut(path string, handler restfulHandler, middlewares ...MiddlewareFunc) *Route {
-	return g.restful(http.MethodPut, path, handler, http.StatusOK, http.StatusBadRequest, middlewares...)
+	return g.restful(
+		http.MethodPut, path, handler,
+		http.StatusOK, http.StatusBadRequest,
+		middlewares...,
+	)
 }
 
 func (g *Group) RestfulDelete(path string, handler restfulHandler, middlewares ...MiddlewareFunc) *Route {
-	return g.restful(http.MethodDelete, path, handler, http.StatusNoContent, http.StatusBadRequest, middlewares...)
+	return g.restful(
+		http.MethodDelete, path, handler,
+		http.StatusNoContent, http.StatusBadRequest,
+		middlewares...,
+	)
 }
 
 func (g *Group) restful(method string, path string, handler restfulHandler, successCode int, failedCode int, middlewares ...MiddlewareFunc) *Route {
