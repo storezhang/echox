5b4703a30db46e233546bba857eb441d58cf7d27
storezhang <storezhang@gmail.com>
2021-07-28 15:54:13 +0800

    处理默认值

--- binder.go
+++ binder.go
@@ -8,13 +8,13 @@ import (
 type binder struct{}
 
 func (b *binder) Bind(data interface{}, ctx echo.Context) (err error) {
-	// 处理默认值
-	defaults.SetDefaults(data)
-
 	db := new(echo.DefaultBinder)
 	if err = db.Bind(data, ctx); err != echo.ErrUnsupportedMediaType {
 		return
 	}
+
+	// 处理默认值
+	defaults.SetDefaults(data)
 
 	return
 }
